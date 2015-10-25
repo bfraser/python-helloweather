5a167386d07b996e035ec72cb13f6409e0a291e9
Bill Fraser <fraser@pythian.com>
2015-10-25 01:09:18 -0400

    OpenWeatherMap now requires an API key, switching to Weather.gov

--- helloweather.py
+++ helloweather.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python
 import urllib2
 import json
-from flask import Flask,render_template
+from flask import Flask, render_template
 import pdb
 import platform
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def helloweather():
     # Return my IP and lon/lat
-    req=urllib2.Request('http://ipinfo.io')
-    req.add_header('User-Agent','curl')
-    response=urllib2.urlopen(req)
-    geoip_params=""
+    req = urllib2.Request('http://ipinfo.io')
+    req.add_header('User-Agent', 'curl')
+    response = urllib2.urlopen(req)
+    geoip_params = ''
     try:
-        geoip_params=json.loads(response.read())
+        geoip_params = json.loads(response.read())
     except:
         print "Couldn't retrieve your IP / location"
 
-    lat,lon = geoip_params['loc'].split(',')
-    print lon,lat
-    weatherurl = "http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s" % (lat,lon)
+    lat, lon = geoip_params['loc'].split(',')
+    print lon, lat
+    weatherurl = "http://forecast.weather.gov/MapClick.php?lat=%s&lon=%s&FcstType=json" % (lat, lon)
     weatherdata = urllib2.urlopen(weatherurl)
-    dictweatherdata=""
+    dictweatherdata = ''
     try:
         dictweatherdata = json.loads(weatherdata.read())
     except:
         print "Couldn't retrieve weather in your area :("
-    return render_template('hello.html',weatherdata=dictweatherdata,hostname=platform.node())
+    return render_template('hello.html', weatherdata=dictweatherdata, hostname=platform.node())
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port=5555)
-
-
+    app.run(debug=True, host="0.0.0.0", port=5555)
